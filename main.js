248fffbcf694f9ae44c684b2137dad22dd78a436
Torrecao <joaq.becerra@duocuc.cl>
2026-07-29 23:40:57 -0400

    cambios añadidos al historial del repositorio

---
+++ main.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow, ipcMain  } = require("electron");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 292,
+    height: 350,
+    resizable: false,
+    maximizable: false,
+    fullscreenable: false,
+    frame: false, 
+    transparent: false,
+    backgroundColor: "#453732",
+    webPreferences: {
+      contextIsolation: true
+    }
+  });
+
+  win.loadFile("index.html");
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
